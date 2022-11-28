e2d59017897571861dd87b7eb44bdbf73eafc0a7
Bruno Molezzi <bru.molezzi92@gmail.com>
2022-11-28 18:45:31 -0300

    ECOM IORI - AJUSTAR FUNCIONES

---
+++ js/app.js
@@ -0,0 +1,156 @@
+// PRODUCTOS
+const productos = [
+    {
+        id:"Anillo 01",
+        titulo:"Anillo Plata",
+        img:"./img/anillos/anillo01.jpg",
+        categoria: {
+            nombre:"Anillos",
+            id: "Anillos"
+        },
+        precio:1500
+    },
+    {
+        id:"Anillo 02",
+        titulo:"Anillo Oro",
+        img:"./img/anillos/anillo01.jpg",
+        categoria: {
+            nombre:"Anillos",
+            id: "Anillos"
+        },
+        precio:1500
+    },
+    {
+        id:"Anillo 03",
+        titulo:"Anillo Plata y Oro",
+        img:"./img/anillos/anillo01.jpg",
+        categoria: {
+            nombre:"Anillos",
+            id: "Anillos"
+        },
+        precio:1500
+    },
+    {
+        id:"Reloj 01",
+        titulo:"Reloj Casio",
+        img:"./img/anillos/anillo01.jpg",
+        categoria: {
+            nombre:"Relojes",
+            id: "Reloj"
+        },
+        precio:1500
+    },
+    {
+        id:"Reloj 02",
+        titulo:"Reloj Casio Mujer",
+        img:"./img/anillos/anillo01.jpg",
+        categoria: {
+            nombre:"Relojes",
+            id: "Reloj"
+        },
+        precio:1500
+    },
+    {
+        id:"Cadena 01",
+        titulo:"Cadena de Plata",
+        img:"./img/anillos/anillo01.jpg",
+        categoria: {
+            nombre:"Cadenas",
+            id: "Cadena"
+        },
+        precio:1500
+    },
+]
+
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.img}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
